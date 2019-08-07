3100969bcba426e3c8b42a8ef963dd69dc26eeeb
crocodeev <svin862003@mail.ru>
2019-08-07 12:20:07 +0300

    fixed mistake

--- custoModules/messageHandler.js
+++ custoModules/messageHandler.js
@@ -43,7 +43,7 @@ function emo (message) {
 
 function format (place, time, message){
   let picture = emo(message);
-  return phrase = picture + " " + place + " " + message + " " + date;
+  return phrase = picture + " " + place + " " + message + " " + time;
 }
 
 module.exports.emo = emo;
