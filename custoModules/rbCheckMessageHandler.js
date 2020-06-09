8e772bce20449fcfe2fd6e6ab5bebebf03c83fd8
crocodeev <svin862003@mail.ru>
2020-06-09 15:39:01 +0300

    test3 new api final

---
+++ custoModules/rbCheckMessageHandler.js
@@ -0,0 +1,22 @@
+function rbCheckMessageHandler(object) {
+
+  let strings = object.strings;
+  let message;
+
+  message = object.name;
+  message = message + "\n" + object.message;
+  message = message + "\n";
+
+  strings.forEach((item) => {
+    message = message + "\n" + item;
+  });
+
+  message = message + "\n";
+  message = message + "\n" + object.total;
+  message = message + "\n" + object.timing;
+
+  return message;
+
+}
+
+module.exports.format = rbCheckMessageHandler;
