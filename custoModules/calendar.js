f1aec196e82cbaff669e0d8f94fa0d64787f3115
crocodeev <svin862003@mail.ru>
2020-06-16 11:04:17 +0300

    add undefined exeption

--- custoModules/calendar.js
+++ custoModules/calendar.js
@@ -48,19 +48,20 @@ function getEvents(config,when,where) {
   .then(
     (message) => {
 
-      console.log(message);
-
-      switch (where) {
-        case "spb":
-        bot.sendMessage(settings.spbChatID, message);
-        break;
-        case "msk":
-        bot.sendMessage(settings.mskChatID, message);
-        break;
-        default:
-        bot.sendMessage(settings.testChatID, message);
+      if (typeof message === 'undefined') {
+        console.log("really nothing to do");
+      } else {
+        switch (where) {
+          case "spb":
+          bot.sendMessage(settings.spbChatID, message);
+          break;
+          case "msk":
+          bot.sendMessage(settings.mskChatID, message);
+          break;
+          default:
+          bot.sendMessage(settings.testChatID, message);
+        }
       }
-
     }
   )
   .catch(
