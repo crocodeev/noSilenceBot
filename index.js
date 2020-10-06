55146fcc711e6f39440cff9d3e215bf4e5d30de8
crocodeev <svin862003@mail.ru>
2020-10-06 15:38:49 +0300

    test 06102020

--- index.js
+++ index.js
@@ -64,5 +64,5 @@ getTodayEventsSPB.start();
 getInWeekEventsSPB.start();
 getTodayEventsMSK.start();
 getInWeekEventsMSK.start();
-//getTodayEventsDUTY.start();
-//getTestEventsTest.start();
+getTodayEventsDUTY.start();
+getTestEventsTest.start();
